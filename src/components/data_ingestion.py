c458e9dfbc06708db6222fabb36df2957c37593d
Prajwal Venkatesh <prajwal@Prajwals-MacBook-Air.local>
2025-08-18 23:12:11 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,7 @@ from src.components.data_transformation import (
     DataTransformation,
     DataTransformationConfig,
 )
+from src.components.model_trainer import ModelTrainerConfig, ModerTrainer
 
 
 @dataclass
@@ -61,4 +62,9 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_data, test_data
+    )
+
+    model_trainer = ModerTrainer()
+    print(model_trainer.initate_model_training(train_arr, test_arr))
